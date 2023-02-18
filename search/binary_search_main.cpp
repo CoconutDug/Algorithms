a1eb63fd7078b5ce8b55df92a74b1782e4cca4b1
CoconutDug <gabriel.esqueda8630@alumnos.udg.mx>
2023-02-17 18:32:59 -0600

    Arreglado error de task para compilar binary_search_main.cpp

--- search/binary_search_main.cpp
+++ search/binary_search_main.cpp
@@ -2,37 +2,7 @@
 #include "../utilities/files_utilities/file_utilities.h"
 #include "../utilities/vector_utilities/utilities_vectors.h"
 #include <vector>
-
-int binary_search_recur(std::vector<int> &v, int r, int l, int b)
-{
-    int mid = (l + r) / 2;
-    if (b == v[mid])
-        return mid;
-    if (l == r)
-        return -1;
-    if (b < v[mid])
-        l = mid;
-    else
-        r = mid + 1;
-
-    return binary_search_recur(v, r, l, b);
-}
-int binary_search(std::vector<int> &v, int r, int l, int b)
-{
-    int mid;
-    do
-    {
-        mid = (l + r) / 2;
-        if (v[mid] == b)
-            return mid;
-        if (b < v[mid])
-            l = mid;
-        else
-            r = mid + 1;
-    } while (mid != r);
-
-    return -1;
-}
+#include "searching.h"
 
 int main()
 {
